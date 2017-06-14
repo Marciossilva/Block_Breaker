73392a8b86fbe1223f0de3554f31e3904164265e
Marcio Silva <marcioadsworks@gmail.com>
2017-06-14 09:15:00 -0300

    iniciado e com menu ja feito import

---
+++ Assets/Scripts/MusicaFundo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MusicaFundo : MonoBehaviour {
+
+	static MusicaFundo instance = null;
+
+	void Awake(){
+
+		if (instance != null) {
+			Destroy (gameObject);
+
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
