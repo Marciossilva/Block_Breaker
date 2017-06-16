2404e96d521c8bf41720f09ed64475933b736efa
Marcio Silva <marcioadsworks@gmail.com>
2017-06-16 12:28:50 -0300

    collider funcionando

---
+++ Assets/Scripts/looseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class looseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D trigger){
+		Debug.Log ("passou");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+	
+		Debug.Log ("bateu");
+	}
+}
