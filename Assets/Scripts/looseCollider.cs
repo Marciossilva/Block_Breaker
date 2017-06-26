5690ab8095665986ddbcf18c180010193d031489
Marcio Silva <marcioadsworks@gmail.com>
2017-06-26 16:48:35 -0300

    bola pulando e pedal funcionando com o mouse

--- Assets/Scripts/looseCollider.cs
+++ Assets/Scripts/looseCollider.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class looseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
 	// Use this for initialization
 	void OnTriggerEnter2D(Collider2D trigger){
-		Debug.Log ("passou");
+		print ("passou");
+
+		levelManager.LoadLevel ("Lose");
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
