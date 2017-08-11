e71c829cd752469a355b73cd115adc7eda543dd8
Marcio Silva <marcioadsworks@gmail.com>
2017-08-11 13:14:00 -0300

    mudanca de sprites

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+
+	public Sprite[] hitSprites;
+
 	private int timesHit;
 	private LevelManager levelManager;
 
@@ -21,9 +23,28 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col){
+		bool isBreakable = (this.tag == "Breakable");
+		if(isBreakable){
+		HandleHits ();
+		}
+	}
+
+	void HandleHits(){
 		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+
 		if (timesHit >= maxHits) {
 			Destroy (gameObject);
+		} else {
+			LoadSprites ();		
+		}
+	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit - 1;
+		if(hitSprites [spriteIndex]){
+			this.GetComponent < SpriteRenderer> ().sprite = hitSprites [spriteIndex]; 
+		
 		}
 	}
 
